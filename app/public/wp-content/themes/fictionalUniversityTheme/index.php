141a78448e8b63bcab4f7bc9d4920c826403c799
wcalvert88 <wcalvert88@gmail.com>
2018-08-08 19:45:44 -0400

    made index, single files, and pages for wp

--- app/public/wp-content/themes/fictionalUniversityTheme/index.php
+++ app/public/wp-content/themes/fictionalUniversityTheme/index.php
@@ -1,11 +1,9 @@
-<?php 
-  function greet($name, $color) {
-		echo "<p>Hi, my name is $name and my favorite color is $color.</p>";
-	}
+<?php
+while(have_posts()) {
+	the_post(); ?>
+	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	<?php the_content() ?>
+	<hr>
+<?php }
 
-greet('John', 'blue');
-greet('Jane', 'green');
 ?>
-
-<h1><?php bloginfo('name'); ?></h1>
-<p><?php bloginfo('description'); ?></p>
