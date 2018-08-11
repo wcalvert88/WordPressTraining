8aab523e45c414337951019f09c2964dcca78a80
wcalvert88 <wcalvert88@gmail.com>
2018-08-11 14:27:50 -0400

    added my own custom Event post type

--- app/public/wp-content/themes/fictionalUniversityTheme/functions.php
+++ app/public/wp-content/themes/fictionalUniversityTheme/functions.php
@@ -16,5 +16,5 @@ function university_features() {
     add_theme_support('title-tag');
 }
 
-add_action('after_setup_theme', 'university_features')
+add_action('after_setup_theme', 'university_features');
 ?>
