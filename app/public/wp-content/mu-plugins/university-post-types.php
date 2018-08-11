ccfea6888b8e90291d5fd21f549804ea730947c2
wcalvert88 <wcalvert88@gmail.com>
2018-08-11 14:58:35 -0400

    made my events pages work

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,13 +1,15 @@
 <?php 
 function university_post_types() {
     register_post_type('event', array(
+        'rewrite' => array('slug' => 'events'),
+        'has_archive' => true,
         'public' => true,
         'labels' => array(
             'name' => 'Events',
             'add_new_item' => 'Add New Event',
             'edit_item' => 'Edit Event',
             'all_items' => 'All Events',
-            'signular_name' => 'Event'
+            'singular_name' => 'Event'
         ),
         'menu_icon' => 'dashicons-calendar'
     ));
