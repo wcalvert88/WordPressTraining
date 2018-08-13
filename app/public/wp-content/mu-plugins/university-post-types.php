995f99fdd2b5a062448478014af0f46f8cf9e1d2
wcalvert88 <wcalvert88@gmail.com>
2018-08-13 15:29:51 -0400

    added the professor type and got it related to programs

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -32,6 +32,20 @@ function university_post_types() {
         ),
         'menu_icon' => 'dashicons-awards'
     ));
+
+     // Professor Post Type
+     register_post_type('professor', array(
+        'supports' => array('title', 'editor'),
+        'public' => true,
+        'labels' => array(
+            'name' => 'Professor',
+            'add_new_item' => 'Add New Professor',
+            'edit_item' => 'Edit Professor',
+            'all_items' => 'All Professor',
+            'singular_name' => 'Professor'
+        ),
+        'menu_icon' => 'dashicons-welcome-learn-more'
+    ));
 }
 
 add_action('init', 'university_post_types');
