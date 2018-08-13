520779e4ddf3a23b47131defa73db6ee2f5891e6
wcalvert88 <wcalvert88@gmail.com>
2018-08-13 14:52:24 -0400

    fixed a couple typos and finished related programs section

--- app/public/wp-content/themes/fictionalUniversityTheme/functions.php
+++ app/public/wp-content/themes/fictionalUniversityTheme/functions.php
@@ -22,7 +22,6 @@ function university_adjust_queries($query) {
     if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
         $query->set('orderby', 'title');
         $query->set('order', 'ASC');
-        $query-set('posts_per_page', 1);
     }
 
     if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
