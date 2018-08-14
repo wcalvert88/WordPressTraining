d48d5d93a2dfad43dc031faa1987cbf350c27eba
wcalvert88 <wcalvert88@gmail.com>
2018-08-14 15:49:38 -0400

    add Manual Image Crop plugin to customize cropping of images

--- app/public/wp-content/themes/fictionalUniversityTheme/single-program.php
+++ app/public/wp-content/themes/fictionalUniversityTheme/single-program.php
@@ -44,7 +44,7 @@ if ($relatedProfessors->have_posts()) {
   while($relatedProfessors->have_posts()) {
       $relatedProfessors->the_post();
       ?>
-  <li class="professor-card__list-item"><a class="professor-card" href="<?php the_permalink(); ?>"><img class="professor-card__image" src="<?php the_post_thumbnail_url(); ?>"><span class="professor-card__name"><?php the_title(); ?></span></a></li>
+  <li class="professor-card__list-item"><a class="professor-card" href="<?php the_permalink(); ?>"><img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>"><span class="professor-card__name"><?php the_title(); ?></span></a></li>
   <?php
   }
   echo '</ul>';
