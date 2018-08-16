34bf1d135d44e2852eefe47c4e8ca68908ca31ae
wcalvert88 <wcalvert88@gmail.com>
2018-08-15 20:13:29 -0400

    got google map running on the campus archive page

--- app/public/wp-content/themes/fictionalUniversityTheme/functions.php
+++ app/public/wp-content/themes/fictionalUniversityTheme/functions.php
@@ -29,6 +29,7 @@ function pageBanner($args = NULL) {
 <?php }
 
 function university_files() {
+    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyAcWPtg3sMeIl46c28LvDDas979r73J_ZU', NULL, '1.0', true);
     wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
