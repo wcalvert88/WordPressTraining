137f6573e8a808f98cb9b6fd4925a71a020710fd
wcalvert88 <wcalvert88@gmail.com>
2018-08-18 11:41:41 -0400

    set a timeout function to run 2 seconds after a key is pressed to get ready for live searching

--- app/public/wp-content/themes/fictionalUniversityTheme/js/modules/Search.js
+++ app/public/wp-content/themes/fictionalUniversityTheme/js/modules/Search.js
@@ -5,8 +5,10 @@ class Search {
         this.openButton = $(".js-search-trigger");
         this.closeButton = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
+        this.searchField = $("#search-term");
         this.events();
         this.isOverlayOpen = false;
+        this.typingTimer;
     }
 
     // 2. events or dispatcher
@@ -14,9 +16,15 @@ class Search {
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
         $(document).on("keyup", this.keyPressDispatcher.bind(this));
+        this.searchField.on("keydown", this.typingLogic.bind(this));
     }
 
     // 3. methods (function, action)
+    typingLogic() {
+        clearTimeout(this.typingTimer);
+        this.typingTimer = setTimeout(function () { console.log("This is a timeout test");}, 2000);
+    }
+
     keyPressDispatcher(e) {
         if (e.keyCode == 83 && !this.isOverlayOpen) {
             this.openOverlay();
