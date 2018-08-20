9db342eddd1120a94ca8e0319a509f61e644247e
wcalvert88 <wcalvert88@gmail.com>
2018-08-20 10:28:20 -0400

    got live searching to work

--- app/public/wp-content/themes/fictionalUniversityTheme/js/modules/Search.js
+++ app/public/wp-content/themes/fictionalUniversityTheme/js/modules/Search.js
@@ -44,8 +44,13 @@ class Search {
     }
 
     getResults() {
-        $.getJSON("http://fictional-university.local/wp-json/wp/v2/posts?search=" + this.searchField.val(), function(posts) {
-            alert(posts[0].title.rendered)
+        $.getJSON("http://fictional-university.local/wp-json/wp/v2/posts?search=" + this.searchField.val(), posts => {
+
+                this.resultsDiv.html(`
+                    <h2 class="search-overlay__section-title">General Information</h2>
+                    <ul class="link-list min-list">
+                        ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</li>`).join('')}
+                    </ul>`);
         });
     }
 
