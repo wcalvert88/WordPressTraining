06187ca61534d6371b67b90be389a059a7c0e058
wcalvert88 <wcalvert88@gmail.com>
2018-08-20 19:23:55 -0400

    cleaned up the search made it reset if search is closed and added autofocus to the search box

--- app/public/wp-content/themes/fictionalUniversityTheme/js/modules/Search.js
+++ app/public/wp-content/themes/fictionalUniversityTheme/js/modules/Search.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 class Search {
     // 1. describe and create/initiate our object
     constructor() {
+        this.addSearchHTML();
         this.resultsDiv = $("#search-overlay__results");
         this.openButton = $(".js-search-trigger");
         this.closeButton = $(".search-overlay__close");
@@ -32,7 +33,7 @@ class Search {
                 this.resultsDiv.html('<div class="spinner-loader"></div>');
                 this.isSpinnerVisible = true;
             }
-            this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+            this.typingTimer = setTimeout(this.getResults.bind(this), 750);
             } else {
                 this.resultsDiv.html('');
                 this.isSpinnerVisible = false;
@@ -68,6 +69,8 @@ class Search {
     openOverlay() {
         this.searchOverlay.addClass("search-overlay--active");
         $("body").addClass("body-no-scroll");
+        this.searchField.val('');
+        setTimeout(() => this.searchField.focus(), 301);
         this.isOverlayOpen = true;
     }
     
@@ -75,6 +78,25 @@ class Search {
         this.searchOverlay.removeClass("search-overlay--active");
         $("body").removeClass("body-no-scroll");
         this.isOverlayOpen = false;
+    }
+
+    addSearchHTML() {
+        $("body").append(`
+        <div class="search-overlay">
+        <div class="search-overlay__top">
+          <div class="container">
+          <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="container">
+                    <div id="search-overlay__results"></div>
+        </div>
+      </div>
+    
+    
+        `);
     }
 }
 
