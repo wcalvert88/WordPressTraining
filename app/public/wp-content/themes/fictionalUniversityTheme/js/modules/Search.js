007d4f739e2b49a2ff3870d89d1a4148cff583de
wcalvert88 <wcalvert88@gmail.com>
2018-08-20 19:33:51 -0400

    set up a synchronous way of searching posts and pages

--- app/public/wp-content/themes/fictionalUniversityTheme/js/modules/Search.js
+++ app/public/wp-content/themes/fictionalUniversityTheme/js/modules/Search.js
@@ -46,13 +46,15 @@ class Search {
 
     getResults() {
         $.getJSON(universityData.root_url + "/wp-json/wp/v2/posts?search=" + this.searchField.val(), posts => {
-
+            $.getJSON(universityData.root_url + "/wp-json/wp/v2/pages?search=" + this.searchField.val(), pages => {
+                var combinedResults = posts.concat(pages);
                 this.resultsDiv.html(`
                     <h2 class="search-overlay__section-title">General Information</h2>
-                        ${posts.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>'}
-                        ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</li>`).join('')}
-                    ${posts.length ? '</ul>' : ''}`);
+                        ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>'}
+                        ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</li>`).join('')}
+                    ${combinedResults.length ? '</ul>' : ''}`);
                     this.isSpinnerVisible = false;
+            });
         });
     }
 
@@ -91,11 +93,9 @@ class Search {
           </div>
         </div>
         <div class="container">
-                    <div id="search-overlay__results"></div>
+            <div id="search-overlay__results"></div>
         </div>
       </div>
-    
-    
         `);
     }
 }
