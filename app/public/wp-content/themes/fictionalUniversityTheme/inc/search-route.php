7d11f1147f570414f52fc259acf6266360e5093a
wcalvert88 <wcalvert88@gmail.com>
2018-08-21 14:37:31 -0400

    got keyword searching working with new API

--- app/public/wp-content/themes/fictionalUniversityTheme/inc/search-route.php
+++ app/public/wp-content/themes/fictionalUniversityTheme/inc/search-route.php
@@ -10,9 +10,10 @@ function universityRegisterSearch() {
     ));
 }
 
-function universitySearchResults() {
+function universitySearchResults($data) {
     $professors = new WP_Query(array(
-        'post_type' => 'professor'
+        'post_type' => 'professor',
+        's' => sanitize_text_field($data['term'])
     ));
     $professorResults = array();
 
