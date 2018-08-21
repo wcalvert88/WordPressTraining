4beaa947b3a3ce4322c9e2216a3a9f02f9b61ace
wcalvert88 <wcalvert88@gmail.com>
2018-08-21 13:29:28 -0400

    started to create my own REST API route

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -51,6 +51,7 @@ function university_post_types() {
 
      // Professor Post Type
      register_post_type('professor', array(
+        'show_in_rest' => true,
         'supports' => array('title', 'editor', 'thumbnail'),
         'public' => true,
         'labels' => array(
