908644ea7a9685b445678fe53cacb013128107e2
wcalvert88 <wcalvert88@gmail.com>
2018-08-21 14:24:42 -0400

    modified the new REST API to include the professor post title and permalink

--- app/public/wp-content/themes/fictionalUniversityTheme/inc/search-route.php
+++ app/public/wp-content/themes/fictionalUniversityTheme/inc/search-route.php
@@ -11,6 +11,18 @@ function universityRegisterSearch() {
 }
 
 function universitySearchResults() {
-    return 'Congratulations, you created a route.';
+    $professors = new WP_Query(array(
+        'post_type' => 'professor'
+    ));
+    $professorResults = array();
+
+    while($professors->have_posts()) {
+        $professors->the_post();
+        array_push($professorResults, array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+        ));
+    }
+    return $professorResults;
 };
 ?>
