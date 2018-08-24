892e314bf045c25cc44596ed3ed47eeabb116791
wcalvert88 <wcalvert88@gmail.com>
2018-08-24 19:23:55 -0400

    got delete note to be dynamic and got the edit button and save button started

--- app/public/wp-content/themes/fictionalUniversityTheme/js/modules/MyNotes.js
+++ app/public/wp-content/themes/fictionalUniversityTheme/js/modules/MyNotes.js
@@ -7,17 +7,26 @@ class MyNotes {
 
     events() {
         $(".delete-note").on("click", this.deleteNote.bind());
+        $(".edit-note").on("click", this.editNote.bind());
     }
 
     // Methods will go here
-    deleteNote() {
+    editNote(e) {
+        let thisNote = $(e.target).parents("li");
+        thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass('note-active-field');
+        thisNote.fadeIn(".update-note").addClass("update-note--visible");
+    }
+
+    deleteNote(e) {
+        let thisNote = $(e.target).parents("li");
         $.ajax({
             beforeSend: (xhr) => {
                 xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
             },
-            url: universityData.root_url + '/wp-json/wp/v2/note/93',
+            url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
             type: 'DELETE',
             success: (response) => {
+                thisNote.slideUp();
                 console.log("Congrats");
                 console.log(response);
             },
