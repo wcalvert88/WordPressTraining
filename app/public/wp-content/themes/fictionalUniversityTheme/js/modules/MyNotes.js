a9bfb24408183749535bbb90b589bd8e96698968
wcalvert88 <wcalvert88@gmail.com>
2018-08-24 20:22:01 -0400

    fixed typos and got the edits to save

--- app/public/wp-content/themes/fictionalUniversityTheme/js/modules/MyNotes.js
+++ app/public/wp-content/themes/fictionalUniversityTheme/js/modules/MyNotes.js
@@ -6,15 +6,33 @@ class MyNotes {
     }
 
     events() {
-        $(".delete-note").on("click", this.deleteNote.bind());
-        $(".edit-note").on("click", this.editNote.bind());
+        $(".delete-note").on("click", this.deleteNote.bind(this));
+        $(".edit-note").on("click", this.editNote.bind(this));
+        $(".update-note").on("click", this.updateNote.bind(this));
     }
 
     // Methods will go here
     editNote(e) {
         let thisNote = $(e.target).parents("li");
+        if (thisNote.data("state") == 'editable') {
+            this.makeNoteReadOnly(thisNote);
+        } else {
+            this.makeNoteEditable(thisNote);
+        }
+    }
+
+    makeNoteEditable(thisNote) {
+        thisNote.find(".edit-note").html('<i class="fa fa-times" aria-hidden=true></i> Cancel');
         thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass('note-active-field');
-        thisNote.fadeIn(".update-note").addClass("update-note--visible");
+        thisNote.find(".update-note").addClass("update-note--visible");
+        thisNote.data("state", "editable");
+    }
+
+    makeNoteReadOnly(thisNote) {
+        thisNote.find(".edit-note").html('<i class="fa fa-pencil" aria-hidden=true></i> Edit');
+        thisNote.find(".note-title-field, .note-body-field").attr("readonly", "readonly").removeClass('note-active-field');
+        thisNote.find(".update-note").removeClass("update-note--visible");
+        thisNote.data("state", "cancel");
     }
 
     deleteNote(e) {
@@ -27,6 +45,31 @@ class MyNotes {
             type: 'DELETE',
             success: (response) => {
                 thisNote.slideUp();
+                console.log("Congrats");
+                console.log(response);
+            },
+            error: (response) => {
+                console.log("Sorry");
+                console.log(response);
+            }
+        });
+    }
+
+    updateNote(e) {
+        let thisNote = $(e.target).parents("li");
+        let ourUpdatedPost = {
+            'title': thisNote.find(".note-title-field").val(),
+            'content': thisNote.find(".note-body-field").val()
+        }
+        $.ajax({
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+            },
+            url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
+            type: 'POST',
+            data: ourUpdatedPost,
+            success: (response) => {
+                this.makeNoteReadOnly(thisNote);
                 console.log("Congrats");
                 console.log(response);
             },
--- app/public/wp-content/themes/fictionalUniversityTheme/page-my-notes.php
+++ app/public/wp-content/themes/fictionalUniversityTheme/page-my-notes.php
@@ -27,7 +27,7 @@ while(have_posts()) {
                     <input readonly class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
                     <span class="edit-note"><i class="fa fa-pencil" aria-hidden=true></i>Edit</span>
                     <span class="delete-note"><i class="fa fa-trash-o" aria-hidden=true></i>Delete</span>
-                    <textarea readonly class="note-body-field"><?php echo esc_attr(get_the_content()); ?></textarea>
+                    <textarea readonly class="note-body-field"><?php echo esc_textarea(get_the_content()); ?></textarea>
                     <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden=true></i>Save</span>
                 </li>
 
